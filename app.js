921430c871465c14d48ab3c14d6ed7286b62c657
Mustafa Sungur Öztürk <mustafasungur.ozturkk@gmail.com>
2023-10-21 21:47:28 +0300

    ejs files complated

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const path = require("path");
-
+const ejs = require("ejs");
 const app = express();
 
+// TAMPLATE ENGINE
+app.set("view engine", "ejs");
+
+// MIDDLEWARE
 app.use(express.static("public"));
 
+// ROUTES
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./temp/index.html"));
+  // res.sendFile(path.resolve(__dirname, "./views/index.html"));
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/add", (req, res) => {
+  res.render("add");
 });
 
 const port = 3000;
